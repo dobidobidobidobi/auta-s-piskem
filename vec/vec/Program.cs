1c8d5935746e14af8e6e3e4dbcd23d713bd57126
JoeBiden <dobiasj@gchd.cz>
2024-11-04 16:26:13 +0100

    program

--- vec/vec/Program.cs
+++ vec/vec/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace vec;
+﻿using System;
+using System.Collections.Generic;
+
+namespace vec;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var kalendar = new priorit
     }
+}
+class Car
+{
+    int jmeno { get; }
+    int nosnost { get;  }
+    int nalozdoba { get; }
+    int cesta { get;  }
+    int vylozdoba { get; }
+}
+
+class Udalost
+{
+    Car auto;
+    enum TypUdalosti { };
+    TypUdalosti udalost { get; }
 }
