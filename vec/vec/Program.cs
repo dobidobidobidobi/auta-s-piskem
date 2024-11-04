feff62f3170fd1bc9bc7bed0e5ccdd0278df6c32
Xer0r <nondekry@gmail.com>
2024-11-04 16:33:02 +0100

    kod

--- vec/vec/Program.cs
+++ vec/vec/Program.cs
@@ -1,27 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace vec;
-
-class Program
+namespace ConsoleApp9
 {
-    static void Main(string[] args)
+    class Car
     {
-        var kalendar = new priorit
+        int jmeno { get; }
+        int nosnost { get; }
+        int nalozdoba { get; }
+        int cesta { get; }
+        int vylozdoba { get; }
     }
-}
-class Car
-{
-    int jmeno { get; }
-    int nosnost { get;  }
-    int nalozdoba { get; }
-    int cesta { get;  }
-    int vylozdoba { get; }
-}
 
-class Udalost
-{
-    Car auto;
-    enum TypUdalosti { };
-    TypUdalosti udalost { get; }
+    class Udalost
+    {
+        Car auto;
+        enum TypUdalosti { };
+        TypUdalosti udalost { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
 }
